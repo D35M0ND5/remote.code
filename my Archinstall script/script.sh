3ed20309720c228e6df569bd1f8afc54af828193
Him <145923395+D35M0ND5@users.noreply.github.com>
2025-06-15 19:20:18 +0000

    partition disk function editted and modified main to only implement the partition disk function

--- my Archinstall script/script.sh
+++ my Archinstall script/script.sh
@@ -6,11 +6,11 @@ trap 'echo "Error on line $LINENO"; exit 1' ERR
 
 # Configuration variables
 TARGET_DISK="/dev/nvme0n1"
-HOSTNAME="myarch"
+HOSTNAME="N3M1S1S"
 USERNAME="user"
-TIMEZONE="Europe/London"
-LOCALE="en_US.UTF-8"
-KEYMAP="us"
+TIMEZONE="Africa/Accra"
+LOCALE="en_UK.UTF-8"
+KEYMAP="uk"
 PACKAGES="base linux linux-firmware networkmanager grub efibootmgr sudo git"
 
 get_user_input() {
@@ -23,117 +23,97 @@ get_user_input() {
 }
 
 partition_disk() {
-    echo "Partitioning disk..."
+    echo "Partitioning disk ..."
     
-    # GPT partition table
-    parted -s "$TARGET_DISK" mklabel gpt
+    # Unmount any existing partitions (safety check)
+    umount -R /mnt 2>/dev/null || true
     
-    # EFI partition (512MB)
-    parted -s "$TARGET_DISK" mkpart primary fat32 1MiB 513MiB
-    parted -s "$TARGET_DISK" set 1 esp on
+    # Create GPT partition table
+    printf "g\n" | fdisk "$TARGET_DISK"
     
-    # Root partition (remainder)
-    parted -s "$TARGET_DISK" mkpart primary ext4 513MiB 100%
+    # Create EFI partition (512MB)
+    printf "n\n1\n\n+512M\nt\n1\n" | fdisk "$TARGET_DISK"
+    
+    # Create root partition (remaining space)
+    printf "n\n2\n\n\nw\n" | fdisk "$TARGET_DISK"
+    
+    # Inform kernel about partition changes
+    partprobe "$TARGET_DISK"
     
     # Format partitions
     mkfs.fat -F32 "${TARGET_DISK}p1"
-    mkfs.ext4 "${TARGET_DISK}p2"
+    mkfs.ext4 -F "${TARGET_DISK}p2"
     
     # Mount partitions
     mount "${TARGET_DISK}p2" /mnt
-    mkdir /mnt/boot
+    mkdir -p /mnt/boot
     mount "${TARGET_DISK}p1" /mnt/boot
+    
+    echo "Partitioning completed successfully."
 }
 
 install_base() {
-    echo "Installing base system..."
-    pacstrap /mnt $PACKAGES
+    # echo "Installing base system..."
+    # pacstrap /mnt $PACKAGES
     
-    # Generate fstab
-    genfstab -U /mnt >> /mnt/etc/fstab
+    # # Generate fstab
+    # genfstab -U /mnt >> /mnt/etc/fstab
     
-    # Copy the install script to new system for stage 2
-    cp "$0" /mnt/root/install.sh
-    chmod +x /mnt/install.sh
+    # # Copy the install script to new system for stage 2
+    # cp "$0" /mnt/root/install.sh
+    # chmod +x /mnt/install.sh
 }
 
 configure_system() {
-    echo "Configuring system..."
-    arch-chroot /mnt /bin/bash <<EOF
-    # Set timezone
-    ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
-    hwclock --systohc
+#     echo "Configuring system..."
+#     arch-chroot /mnt /bin/bash <<EOF
+#     # Set timezone
+#     ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+#     hwclock --systohc
     
-    # Set locale
-    sed -i "s/#$LOCALE/$LOCALE/" /etc/locale.gen
-    locale-gen
-    echo "LANG=$LOCALE" > /etc/locale.conf
+#     # Set locale
+#     sed -i "s/#$LOCALE/$LOCALE/" /etc/locale.gen
+#     locale-gen
+#     echo "LANG=$LOCALE" > /etc/locale.conf
     
-    # Set keymap
-    echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
+#     # Set keymap
+#     echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
     
-    # Set hostname
-    echo "$HOSTNAME" > /etc/hostname
+#     # Set hostname
+#     echo "$HOSTNAME" > /etc/hostname
     
-    # Configure hosts file
-    cat > /etc/hosts <<HOSTS
-127.0.0.1   localhost
-::1         localhost
-127.0.1.1   $HOSTNAME.localdomain $HOSTNAME
-HOSTS
+#     # Configure hosts file
+#     cat > /etc/hosts <<HOSTS
+# 127.0.0.1   localhost
+# ::1         localhost
+# 127.0.1.1   $HOSTNAME.localdomain $HOSTNAME
+# HOSTS
     
-    # Install bootloader
-    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-    grub-mkconfig -o /boot/grub/grub.cfg
+#     # Install bootloader
+#     grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+#     grub-mkconfig -o /boot/grub/grub.cfg
     
-    # Enable NetworkManager
-    systemctl enable NetworkManager
+#     # Enable NetworkManager
+#     systemctl enable NetworkManager
     
-    # Create user
-    useradd -m -G wheel -s /bin/bash $USERNAME
-    echo "Set password for $USERNAME:"
-    passwd $USERNAME
+#     # Create user
+#     useradd -m -G wheel -s /bin/bash $USERNAME
+#     echo "Set password for $USERNAME:"
+#     passwd $USERNAME
     
-    # Configure sudo
-    echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
+#     # Configure sudo
+#     echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
     
-    # Run stage 2 of installation
-    /root/install.sh --stage2
-EOF
+#     # Run stage 2 of installation
+#     /root/install.sh --stage2
+# EOF
 }
 
-stage2_install() {
-    echo "Running stage 2 installation..."
-    
-    # Install yay for AUR packages
-    sudo -u $USERNAME bash <<EOF
-    git clone https://aur.archlinux.org/yay.git /tmp/yay
-    cd /tmp/yay
-    makepkg -si --noconfirm
-EOF
-    
-    # Install additional packages
-    yay -S --noconfirm neovim git zsh tmux
-    
-    # Clone dotfiles
-    sudo -u $USERNAME bash <<EOF
-    git clone https://github.com/yourusername/dotfiles.git ~/.dotfiles
-    ~/.dotfiles/install.sh
-EOF
-    
-    # Set zsh as default shell
-    chsh -s /bin/zsh $USERNAME
-}
 
 main() {
     # collect user input variables
     get_user_input
 
-    if [[ "$1" == "--stage2" ]]; then
-        stage2_install
-        exit 0
-    fi
-    
     # Verify boot mode
     if [[ ! -d /sys/firmware/efi ]]; then
         echo "System is not booted in UEFI mode!"
