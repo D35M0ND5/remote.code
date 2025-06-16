aead739fa5be74b5692e9d8310dc7f8fef44b218
Him <145923395+D35M0ND5@users.noreply.github.com>
2025-06-16 21:18:45 +0000

    added the install packages and configure systems functions

--- myArchinstallScript/script.sh
+++ myArchinstallScript/script.sh
@@ -12,7 +12,7 @@ USERNAME="user"
 TIMEZONE="Africa/Accra"
 LOCALE="en_UK.UTF-8"
 KEYMAP="uk"
-PACKAGES="base linux linux-firmware networkmanager grub efibootmgr sudo git"
+PACKAGES="base linux linux-firmware networkmanager grub efibootmgr sudo git nano base-devel"
 
 get_user_input() {
     read -p "Is the drive nvme? ('y' or 'n'): " IS_NVME
@@ -66,62 +66,58 @@ partition_disk() {
     echo "Partitioning completed successfully."
 }
 
-# install_base() {
-#     echo "Installing base system..."
-#     pacstrap /mnt $PACKAGES
+install_base() {
+    echo "Installing base system..."
+    pacstrap -K /mnt $PACKAGES
     
-#     # Generate fstab
-#     genfstab -U /mnt >> /mnt/etc/fstab
+    # Generate fstab
+    genfstab -U /mnt >> /mnt/etc/fstab
     
-#     # Copy the install script to new system for stage 2
-#     cp "$0" /mnt/root/install.sh
-#     chmod +x /mnt/install.sh
-# }
+}
 
-# configure_system() {
-#     echo "Configuring system..."
-#     arch-chroot /mnt /bin/bash <<EOF
-#     # Set timezone
-#     ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
-#     hwclock --systohc
+configure_system() {
+    echo "Configuring system..."
+    arch-chroot /mnt /bin/bash <<EOF
+
+    # Set timezone
+    ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+    hwclock --systohc
     
-#     # Set locale
-#     sed -i "s/#$LOCALE/$LOCALE/" /etc/locale.gen
-#     locale-gen
-#     echo "LANG=$LOCALE" > /etc/locale.conf
+    # Set locale
+    sed -i "s/#$LOCALE/$LOCALE/" /etc/locale.gen
+    locale-gen
+    echo "LANG=$LOCALE" > /etc/locale.conf
     
-#     # Set keymap
-#     echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
+    # Set keymap
+    echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
     
-#     # Set hostname
-#     echo "$HOSTNAME" > /etc/hostname
+    # Set hostname
+    echo "$HOSTNAME" > /etc/hostname
     
-#     # Configure hosts file
-#     cat > /etc/hosts <<HOSTS
-# 127.0.0.1   localhost
-# ::1         localhost
-# 127.0.1.1   $HOSTNAME.localdomain $HOSTNAME
-# HOSTS
+    # Configure hosts file
+    cat > /etc/hosts <<HOSTS
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   $HOSTNAME.localdomain $HOSTNAME
+HOSTS
     
-#     # Install bootloader
-#     grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-#     grub-mkconfig -o /boot/grub/grub.cfg
+    # Install bootloader
+    grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+    grub-mkconfig -o /boot/grub/grub.cfg
     
-#     # Enable NetworkManager
-#     systemctl enable NetworkManager
+    # Enable NetworkManager
+    systemctl enable NetworkManager
     
-#     # Create user
-#     useradd -m -G wheel -s /bin/bash $USERNAME
-#     echo "Set password for $USERNAME:"
-#     passwd $USERNAME
+    # Create user
+    useradd -m -G wheel -s /bin/bash $USERNAME
+    echo "Set password for $USERNAME:"
+    passwd $USERNAME
     
-#     # Configure sudo
-#     echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
+    # Configure sudo
+    echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
     
-#     # Run stage 2 of installation
-#     /root/install.sh --stage2
-# EOF
-# }
+EOF
+}
 
 
 main() {
@@ -137,7 +133,8 @@ main() {
     
     # # Update mirrorlist
     # echo "Updating mirrorlist..."
-    # reflector --country UK --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+    reflector --country Ghana --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+    # rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
     
     partition_disk
     # install_base
